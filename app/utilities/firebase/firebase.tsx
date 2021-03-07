2dd1bedff3be37102294ba4fa2cace281a9ed926
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2021-03-06 21:01:45 -0500

    Update firebase.tsx

--- app/utilities/firebase/firebase.tsx
+++ app/utilities/firebase/firebase.tsx
@@ -4,4 +4,4 @@ import firebaseConfig from "../../env/firebaseConfig.json"
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-export default firebase;
+export default firebase.apps.length ? firebase.app() : firebase.initializeApp(firebaseConfig);
